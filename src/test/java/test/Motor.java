d44378d30016c55811457b2007bdf2891bfc650f
Juan Tobon <jujotovil@gmail.com>
2024-02-20 11:41:12 -0500

    solución final?

--- src/test/java/test/Motor.java
+++ src/test/java/test/Motor.java
@@ -1,5 +1,20 @@
 package test;
 
 public class Motor {
+	int numeroCilindros;
+	String tipo;
+	int registro;
+	
+	void cambiarRegistro(int registro) {
+		
+		this.registro = registro;
+	}
+	
+	void asignarTipo(String tipo) {
+		
+		if(tipo == "gasolina" || tipo == "electrico") {
+			this.tipo = tipo;
+		}
+	}
 
 }
